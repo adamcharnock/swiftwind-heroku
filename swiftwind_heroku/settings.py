376637dc1dd1c861a0fa7d4117e4da9d15955e92
Adam Charnock <adam@adamcharnock.com>
2017-02-27 17:22:31 +0000

    Setting static root, not creating user on dokku deploy

--- swiftwind_heroku/settings.py
+++ swiftwind_heroku/settings.py
@@ -14,6 +14,7 @@ import os
 
 import dj_database_url
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -123,6 +124,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '.static')
 
 LOGIN_URL = '/auth/login/'
 
